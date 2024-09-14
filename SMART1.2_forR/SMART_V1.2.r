c65ff5074c45a35369d53c98e71c7d7f84292020
Yu-Jen Liang <lyj626@stat.sinica.edu.tw>
2024-09-15 01:05:24 +0800

    Delete SMART1.2_forR directory

--- SMART1.2_forR/SMART_V1.2.r
+++
@@ -1,6 +0,0 @@
-
-cat("Welcome to SMART software (version 1.2): \n")
-cat("Please provide the full path to the SMART programs:\n")
-wd <- scan(n=1, what="character")
-setwd(wd)
-source("SMART_V1.2_Gui.r")
